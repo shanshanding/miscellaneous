a05ae3ced09547bc137f859e3f956eb37ebf1295
shanshanding <sd2204@columbia.edu>
2015-10-21 00:58:32 -0400

    add lower bound simulation

--- worldSeriesDroughts.py
+++ worldSeriesDroughts.py
@@ -2,6 +2,7 @@
 
 from random import randint
 from datetime import date
+import numpy as np
 
 def getNumTeams(endYear):
 # year-by-year number of teams courtesy of
@@ -55,16 +56,36 @@ def droughtProbSpecific(startYear, endYear, numDroughts, condOnCubsDrought=False
     
     probList = [float(num-numDroughts)/num for num in numTeams.values()]
     return(reduce(lambda x, y: x*y, probList))
+
+def droughtProbOriginal(startYear, endYear, numDroughts, numSims, condOnCubsDrought=False):
+    if endYear>date.today().year or startYear<1901 or startYear>endYear or startYear>1960:
+        return -1
+
+    numTeams = 16
+    if condOnCubsDrought:
+        numTeams = 15
+
+    originalTeams = [i for i in range (1, numTeams+1)]
+
+    c=0
+    for i in range(0,numSims):
+        champions = np.random.choice(numTeams, endYear-startYear+1)+1
+        if len([i for i in originalTeams if i not in champions])>=numDroughts:
+            c+=1
+    return(float(c)/numSims)
     
 def main():
     print(droughtProbSpecific(1908, 2014, 1)) 
-    print(droughtProbAny(1908, 2014, 1, 100000)) #Cubs
+    print(droughtProbAny(1908, 2014, 1, 100000)) #one team's 107-year drought 
     print(droughtProbSpecific(1919, 2003, 2))
-    print(droughtProbAny(1919, 2003, 2, 100000)) #Cubs and Red Sox
-    print(droughtProbAny(1919, 2003, 3, 300000)) #Cubs, Red Sox, and White Sox
+    print(droughtProbAny(1919, 2003, 2, 100000)) #two team's 85-year droughts
+    print(droughtProbAny(1919, 2003, 3, 300000)) #three team's 85-year droughts
 
     print(droughtProbSpecific(1949, 2014, 1, True))
-    print(droughtProbAny(1949, 2014, 1, 100000, True)) #Indians
+    print(droughtProbAny(1949, 2014, 1, 100000, True)) #one team's 66-year drought conditioning on the Cubs's drought
+
+    print(droughtProbOriginal(1908, 2014, 1, 300000))   #one team's 107-year drought
+                                                        #if expansion teams have had zero chance of winning 
     
 if __name__ == "__main__":
     main()
